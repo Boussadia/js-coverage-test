1dd8d5aac62c0e02f932c5286a384ac1ecd066f9
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-12-09 17:06:10 +0100

    Updating all process

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,14 @@ module.exports = function(grunt) {
   var childProcess = require('child_process');
   var JSCOVER_PORT = "9999";
   var JAVA_HOME = process.env.JAVA_HOME;
+ function doneFunction(error, result, code) {
+      grunt.log.writeln(error);
+      grunt.log.writeln(result);
+      grunt.log.writeln(code);
+    }
 
-
-  grunt.registerTask('jscover_server','Running JSCover Server', function(){
-    var phantomjs;
+  grunt.registerTask('test','Running JSCover Server', function(){
+    var phantomjs, runnerPJS, converter,jsCoverProc;
     // if there's already phantomjs instance tell it to quit
     phantomjs && phantomjs.kill();
     var done = this.async();
@@ -38,19 +42,11 @@ module.exports = function(grunt) {
           , process.stout
           , process.stderr
           ]},
-      // If this value is set and an error occurs, it will be used as the value
-      // and null will be passed as the error value.
-      fallback: function(e){
-        grunt.log.writeln('erre')
-      }
     };
-    function doneFunction(error, result, code) {
-      grunt.log.writeln(error);
-      grunt.log.writeln(result);
-      grunt.log.writeln(code);
-      done();
-    }
-    var jsCoverProc = grunt.util.spawn(options, doneFunction);
+   
+    jsCoverProc = grunt.util.spawn(options, function(){
+      done()
+    });
     jsCoverProc.stdout.on('data', function(buf) {
       grunt.log.writeln(String(buf));
     });
@@ -59,13 +55,14 @@ module.exports = function(grunt) {
     var binPath = phantomjs.path;
 
     var childArgs = [
-    '--debug=true',
+      '--debug=true',
             jasmine_path,
             url,
             
         ];
 
-    var runner = grunt.util.spawn({
+    setTimeout(function(){
+      runnerPJS = grunt.util.spawn({
         // The command to execute. It should be in the system path.
         cmd: "phantomjs",
         // If specified, the same grunt bin that is currently running will be
@@ -78,25 +75,59 @@ module.exports = function(grunt) {
         opts: { stdio: [ process.stdin
             , process.stout
             , process.stderr
-            ]},
-        // If this value is set and an error occurs, it will be used as the value
-        // and null will be passed as the error value.
-        fallback: function(e){
-          grunt.log.writeln('erre')
-        }
-      },doneFunction);
+            ]}
+      },function (code) {
+          // Tests have finished, so clean up the process
+          var success = (code === 0) ? true : false;
+          jsCoverProc.kill(); // kill the JSCover server now that we are done with it
 
-    runner.stdout.on('data', function(buf) {
-        grunt.log.writeln(String(buf));
-    });
+      });
 
-    runner.on('exit', function (code) {
-        // Tests have finished, so clean up the process
-        var success = (code === 0) ? true : false;
-        jsCoverProc.kill(); // kill the JSCover server now that we are done with it
+      runnerPJS.stdout.on('data', function(buf) {
+          grunt.log.writeln(String(buf));
+      });
 
-        done(success);
-    });
+    }, 1000)
+  })
+
+  grunt.registerTask('convert','Converting ', function(){
+    var converter;
+    var done = this.async();
+    var converterOptions = [
+      '-cp',
+      'jscover\\JSCover-all.jar',
+      'jscover.report.Main',
+      '--format=LCOV',
+      'target\\phantom',
+      'src'
+    ]
+
+      converter = grunt.util.spawn({
+          // The command to execute. It should be in the system path.
+          cmd: "java",
+          // If specified, the same grunt bin that is currently running will be
+          // spawned as the child command, instead of the "cmd" option. Defaults
+          // to false.
+          grunt: false,
+          // An array of arguments to pass to the command.
+          args: converterOptions,
+          // Additional options for the Node.js child_process spawn method.
+          opts: { stdio: [ process.stdin
+              , process.stout
+              , process.stderr
+              ]}
+        },function (code) {
+          // Tests have finished, so clean up the process
+          var success = (code === 0) ? true : false;
+          //runnerPJS.kill(); // kill the JSCover server now that we are done with it
+
+          //done(success);
+      });
+
+      converter.stdout.on('data', function(buf) {
+          grunt.log.writeln(String(buf));
+      });
+
   })
   
   // Project configuration.
@@ -108,13 +139,14 @@ module.exports = function(grunt) {
     clean: {
       src: ['target']
     },
-    jscover_server:{}
+    test:{},
+    convert: {},
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task.
-  grunt.registerTask('default', ['clean', 'jscover_server']);
+  grunt.registerTask('default', ['clean', 'test', 'convert']);
 
 };
